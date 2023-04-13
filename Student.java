c9b9028b516ecca2ce7a9b5994c22549bc41efbf
gqs <aatrox1398147019@163.com>
2023-04-14 02:09:23 +0800

    添加了student类

---
+++ Student.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: GQS
+ * @Date: 2023/4/14-04-14-2:08
+ * @Description: PACKAGE_NAME
+ * @Version 1.0
+ */
+public class Student {
+    private  int sno;
+    private  double height;
+    private  double score;
+}
